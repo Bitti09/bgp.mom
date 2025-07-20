f9066bbe31058347e640bf4473fa9e516676d909
Bitti <Bitti09@users.noreply.github.com>
2025-07-20 13:51:26 +0200

    feat: Add integration for Astro Umami analytics and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,21 @@ import tailwindcss from "@tailwindcss/vite";
 
 import cloudflare from "@astrojs/cloudflare";
 
+import yeskunallumami from "@yeskunall/astro-umami";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+
   vite: {
     plugins: [tailwindcss()],
     build: {
       minify: false,
     },
   },
+
   adapter: cloudflare({ imageService: "compile" }),
+  integrations: [
+    yeskunallumami({ id: "76f95944-8c8c-49e5-accc-025cc8478499" }),
+  ],
 });
